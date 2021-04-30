d4b9e3de7b50ad4911e185cbb3aeeca5bb96c3ef
GDevEngel <tengel403@gmail.com>
2021-04-30 12:59:51 +0200

    cd for laser and enemy created

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] private float _speed = 6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move laser up
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        //destroy laser if off upper screen
+        if (transform.position.y > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,11 @@ public class Player : MonoBehaviour
     [SerializeField] private float _maxPosY = 4f;
     [SerializeField] private float _minPosY = -4.2f;
 
+    [SerializeField] private float _fireRate = 0.25f;
+    private float _nextFire = 0f;
+    private Vector3 _laserOffset = new Vector3(0f, 0.75f, 0f);
+    public GameObject LaserPrefab; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +26,15 @@ public class Player : MonoBehaviour
     void Update()
     {
         PlayerMovement();
+
+        //When fire is pressed AND current game time is greater then previous laser fire time + fire rate(cooldown)
+        if (Input.GetButton("Fire1") && Time.time > _nextFire)
+        {
+            _nextFire = Time.time + _fireRate; 
+            //instantite laser + Y offset
+            Instantiate(LaserPrefab, transform.position +_laserOffset, Quaternion.identity);
+        }
+
     }
 
     private void PlayerMovement()
@@ -28,15 +42,9 @@ public class Player : MonoBehaviour
         //move player based on user input
         transform.Translate(Input.GetAxis("Horizontal") * _speed * Time.deltaTime, Input.GetAxis("Vertical") * _speed * Time.deltaTime, 0);
 
-        //player position restriction on Y axis
-        if (transform.position.y >= _maxPosY)
-        {
-            transform.position = new Vector3(transform.position.x, _maxPosY, 0);
-        }
-        else if (transform.position.y <= _minPosY)
-        {
-            transform.position = new Vector3(transform.position.x, _minPosY, 0);
-        }
+        //player position restriction on Y axis using Mathf.Clamp
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, _minPosY, _maxPosY), 0);
+
         //player position wrap around on X axis
         if (transform.position.x >= _maxPosX)
         {
