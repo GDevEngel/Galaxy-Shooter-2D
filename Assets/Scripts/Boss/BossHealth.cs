e290fb38ff64f374413df1e7006ea2f7e9191d39
GDevEngel <tengel403@gmail.com>
2021-07-15 10:50:02 +0200

    Boss defeat sequence

--- Assets/Scripts/Boss/BossHealth.cs
+++ Assets/Scripts/Boss/BossHealth.cs
@@ -5,23 +5,25 @@ using UnityEngine;
 public class BossHealth : MonoBehaviour
 {
     [SerializeField] GameObject _explosionPrefab;
-
-    private int _bossHealth = 10;
-
+    [SerializeField] GameObject _gameClearText;
+    private Canvas _canvas;
     private AudioSource _audioSource;
-    private float _delay = 7f;
+    private Player _player;
+    
+    [SerializeField] private int _bossHealth = 10;
+    
+    private float _delay = 9f;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        _player = GameObject.FindObjectOfType<Player>().GetComponent<Player>();
+        _canvas = GameObject.FindObjectOfType<Canvas>(); 
+
         _audioSource = GetComponent<AudioSource>();
         _audioSource.PlayDelayed(_delay);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -43,6 +45,10 @@ public class BossHealth : MonoBehaviour
 
     private void BossDeath()
     {
+        _player.AddScore(100);
+        GameObject gameClearText = Instantiate(_gameClearText, _canvas.transform.position, Quaternion.identity);
+        gameClearText.transform.SetParent(_canvas.transform);
+
         //VFX Explosion
         Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
         Destroy(gameObject);
---
+++ Assets/Scripts/GameClearText.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameClearText : MonoBehaviour
+{
+    //handle
+    private AudioSource _audiosource;
+    private Text _text;
+
+    //config
+    private float _BGMduration = 5f;
+    private float _fadeDuration = 5f;
+
+    void Start()
+    {
+        _audiosource = GetComponent<AudioSource>();
+        _audiosource.PlayDelayed(_BGMduration);
+
+        _text = GetComponent<Text>();
+
+        StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        float currentTime = 0f;
+        while (currentTime < _fadeDuration)
+        {
+            float alpha = Mathf.Lerp(0f, 1f, currentTime/_fadeDuration);
+            _text.color = new Color(_text.color.r, _text.color.g, _text.color.b, alpha);
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        yield break;
+    }
+}
